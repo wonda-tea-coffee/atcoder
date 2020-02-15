12849bcc0a6b503166a3fc5db14e8b371064be25
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-15 19:34:00 +0900

    ランダムに置く

--- rcl-contest-2020-qual/a.rb
+++ rcl-contest-2020-qual/a.rb
@@ -1,17 +1,5 @@
-def next_put_pos(grid, c, v)
-  
-end
-
 N, W, K, V = gets.chomp.split.map(&:to_i)
-# 一番右端に置いたモノミノの数の合計を入れる
-grid = Array.new(N){[] * (W + 1)}
 N.times do |i|
-  l = gets.chomp.split.map(&:to_i)
-  c, v = l
-
-  i, j = next_put_pos(grid, h, c, v)
-
-  grid[i][j] = [c, v]
-
-  puts j
+  gets
+  puts (0...W).to_a.sample
 end

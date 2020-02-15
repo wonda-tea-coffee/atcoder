b3c0418e83e64ddc545b84d2f9f11f3ed449bd68
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-15 19:32:34 +0900

    貪欲法途中

--- rcl-contest-2020-qual/a.rb
+++ rcl-contest-2020-qual/a.rb
@@ -1,8 +1,17 @@
+def next_put_pos(grid, c, v)
+  
+end
+
 N, W, K, V = gets.chomp.split.map(&:to_i)
-# c = [] * N
-# v = [] * N
-grid = [] * W
+# 一番右端に置いたモノミノの数の合計を入れる
+grid = Array.new(N){[] * (W + 1)}
 N.times do |i|
   l = gets.chomp.split.map(&:to_i)
-  puts -i % W
+  c, v = l
+
+  i, j = next_put_pos(grid, h, c, v)
+
+  grid[i][j] = [c, v]
+
+  puts j
 end

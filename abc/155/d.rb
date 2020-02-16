f00cc2fd63d9fc6a1a0561bc241516505981190e
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-17 08:43:40 +0900

    finished abc 155

--- abc/155/d.rb
+++ abc/155/d.rb
@@ -1,20 +1,61 @@
 n, k = gets.chomp.split.map(&:to_i)
 a = gets.chomp.split.map(&:to_i)
 
-# 方針①：すべての組み合わせを高速に列挙する
-# 最悪20,000,100,000個
-#
-# 方針②：何らかの方法で1番目,2番目,...,k番目と決めていく
-# やはり最悪k = 20,000,100,000なので厳しい
-#
-# 方針③：他を知らずにk番目だけをピンポイントで求める
+nega = []
+posi = []
+zeros = 0
 
-ps = []
-ng = []
 a.each do |ai|
   if ai < 0
-    ng << ai
+    nega << ai
+  elsif ai > 0
+    posi << ai
   else
-    ps << ai
+    zeros += 1
   end
 end
+
+posi.sort!
+nega.sort!.reverse!
+
+ng = - (10 ** 18) - 1
+ok = 10 ** 18 + 1
+
+while ok - ng > 1
+  mid = (ok + ng) / 2
+  cnt = 0
+
+  if mid >= 0
+    cnt += zeros * (zeros - 1) / 2 + zeros * (n - zeros)
+  end
+
+  if mid > 0
+    cnt += nega.size * posi.size
+    [posi, nega].each do |a|
+      l = 0
+      r = a.size - 1
+      while l < r
+        while l < r and a[l] * a[r] > mid
+          r -= 1
+        end
+        cnt += r - l
+        l += 1
+      end
+    end
+  else
+    r = nega.size - 1
+    0.upto(posi.size - 1) do |l|
+      while r >= 0 and posi[l] * nega[r] <= mid
+        r -= 1
+      end
+      cnt += nega.size - r - 1
+    end
+  end
+
+  if cnt >= k
+    ok = mid
+  else
+    ng = mid
+  end
+end
+puts ok

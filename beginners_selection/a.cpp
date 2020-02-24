35a167cc5b9b89c9629d598c5551650d986b7696
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-02-24 22:43:23 +0900

    beginners selection a

---
+++ beginners_selection/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  string s;
+
+  cin >> a;
+  cin >> b >> c;
+  cin >> s;
+
+  cout << a + b + c << " " + s << endl;
+
+  return 0;
+}

1fb6cd199a682e040412e8bcaff1647d2108e512
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-07 15:18:18 +0900

    solve

---
+++ tenka1-2016-quala/a.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define rep(i,n)   for (int i = 0; i < (n); ++i)
+#define sort(a)    sort((a).begin(), (a).end());
+#define uniq(a)    SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end());
+#define find(a,x)  find((a).begin(), (a).end(), (x)) != (a).end()
+#define reverse(a) reverse((a).begin(), (a).end());
+#define out(d) cout << (d);
+#define outl(d) std::cout<<(d)<<"\n";
+#define Yes() printf("Yes\n");
+#define No() printf("No\n");
+#define YES() printf("YES\n");
+#define NO() printf("NO\n");
+#define ceil(x, y) ((x + y - 1) / (y))
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+
+  ll ans = 0;
+  for (int i = 1; i <= 100; i++) {
+    if (i % 3 != 0 && i % 5 != 0) ans += i;
+  }
+  outl(ans)
+}

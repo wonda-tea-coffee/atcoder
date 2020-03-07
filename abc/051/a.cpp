03121411f717061ee570205fa739c70b661b70e8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-07 16:59:52 +0900

    solve

---
+++ abc/051/a.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define fix(n) cout<<fixed<<setprecision(n);
+#define rep(i,n)   for (int i = 0; i < (n); ++i)
+#define sort(a)    sort((a).begin(), (a).end());
+#define uniq(a)    SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end());
+#define reverse(a) reverse((a).begin(), (a).end());
+#define out(d) cout << (d);
+#define outl(d) std::cout<<(d)<<"\n";
+#define Yes() printf("Yes\n");
+#define No() printf("No\n");
+#define YES() printf("YES\n");
+#define NO() printf("NO\n");
+#define ceil(x, y) ((x + y - 1) / (y))
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+// sの中に含まれるfromを全てtoに置換する
+void replaceAll(string &s, string from, string to) {
+  int pos;
+  int len = from.size();
+  while ((pos = s.find(from)) != string::npos) {
+    s.replace(pos, len, to);
+  }
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12)
+  
+  string s; cin >> s;
+  replaceAll(s, ",", " ");
+  outl(s)
+}

ac39493b91fae243d2ca368100c7f30e440409e7
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-08 23:57:58 +0900

    solve

---
+++ arc/040/a.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define Yes()           printf("Yes\n")
+#define No()            printf("No\n")
+#define YES()           printf("YES\n")
+#define NO()            printf("NO\n")
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+void solve() {
+  int n; cin >> n;
+  string s;
+  rep(i, n) {
+    string tmp; cin >> tmp;
+    s += tmp;
+  }
+  int cntr = 0, cntb = 0;
+  for (int i = 0; i < n * n; i++) {
+    if (s[i] == 'R') cntr++;
+    else if (s[i] == 'B') cntb++;
+  }
+  if (cntr > cntb) outl("TAKAHASHI");
+  else if (cntr < cntb) outl("AOKI");
+  else outl("DRAW");
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

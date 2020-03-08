86d3798d568b4f6cdffe950410fad90fa0f002d9
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-08 12:36:16 +0900

    solve

--- abc/158/e/e.cpp
+++ abc/158/e/e.cpp
@@ -43,4 +43,30 @@ int main() {
 
   ll n, p; cin >> n >> p;
   string s; cin >> s;
+
+  if (10 % p == 0) {
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+      if ((s[i] - '0') % p == 0) ans += i + 1;
+    }
+    outl(ans)
+    return 0;
+  }
+
+  vector<int> d(n + 1);
+  int ten = 1;
+  for (int i = n-1; i >= 0; --i) {
+    int a = (s[i]-'0') * ten % p;
+    d[i] = (d[i+1]+a) % p;
+    ten *= 10; ten %= p;
+  }
+
+  ll ans = 0;
+  vector<int> cnt(p);
+  for (int i = n; i >= 0; --i) {
+    ans += cnt[d[i]];
+    cnt[d[i]]++;
+  }
+
+  outl(ans)
 }

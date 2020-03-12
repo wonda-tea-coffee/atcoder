a6dc6045077ce4c830c18fc70bc67dde897c4b4b
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-12 09:15:25 +0900

    solve

---
+++ abc/059/b.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         SORT(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+int compare(string a, string b) {
+  int size_a = a.size();
+  int size_b = b.size();
+
+  if (size_a < size_b) return -1;
+  else if (size_a > size_b) return 1;
+
+  for (int i = 0; i < size_a; i++) {
+    if (a[i] < b[i]) return -1;
+    else if (a[i] > b[i]) return 1;
+  }
+
+  return 0;
+}
+
+void solve() {
+  string a, b; cin >> a >> b;
+  
+  int c = compare(a, b);
+  if (c == -1) outl("LESS");
+  else if (c == 1) outl("GREATER");
+  else outl("EQUAL");
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

3eb9a7a87312fff73bf89b1b60ade5a89493a3c3
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-14 07:55:48 +0900

    solve

---
+++ tenka1-2013-quala/a.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+// 正の整数n未満の素数を全て求める
+vector<int> sieve(int n) {
+  vector<bool> is_prime(n);
+  rep(i, n) is_prime[i] = true;
+
+  is_prime[1] = false;
+  for (int i = 4; i < n; i += 2) is_prime[i] = false;
+  int lim = int(sqrt(n));
+  for (int i = 3; i <= lim; i += 2) {
+    for (int j = 3; i * j < n; j += 2) {
+      is_prime[i * j] = false;
+    }
+  }
+
+  vector<int> ret;
+  for (int i = 1; i < n; i++) {
+    if (is_prime[i]) ret.push_back(i);
+  }
+  return ret;
+}
+
+void solve() {
+  int n; cin >> n;
+  outl(sieve(n).size());
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

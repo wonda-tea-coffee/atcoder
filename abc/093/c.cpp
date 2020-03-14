9a19efbd049770989b31feb2a3bc8ae5fd82bac3
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-14 17:58:02 +0900

    solve

---
+++ abc/093/c.cpp
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+void solve() {
+  vector<ll> a(3); rep(i, 3) cin >> a[i]; sort(a);
+
+  if (a[0] == a[1] && a[1] == a[2]) outl(0);
+  else if (a[0] == a[1]) outl(a[2] - a[1]);
+  else if (a[1] == a[2]) {
+    if ((a[1] - a[0]) % 2 == 0) outl((a[1] - a[0]) / 2);
+    else outl(ceil(a[1] - a[0], 2) + 1);
+  } else {
+    ll ans = a[2] - a[1];
+    a[1] = a[2];
+    a[0] += ans;
+
+    if ((a[1] - a[0]) % 2 == 0) outl(ans + (a[1] - a[0]) / 2);
+    else outl(ans + ceil(a[1] - a[0], 2) + 1);
+  }
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

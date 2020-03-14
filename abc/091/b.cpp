ba237cfc4b4f63a0b46cd8b86012094723576632
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-14 16:38:41 +0900

    solve

---
+++ abc/091/b.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+
+void solve() {
+  map<string, int> mp;
+
+  int n; cin >> n;
+  rep(i, n) {
+    string s; cin >> s;
+    if (mp.find(s) == mp.end()) {
+      mp.insert(make_pair(s, 1));
+    } else {
+      mp[s]++;
+    }
+  }
+
+  int m; cin >> m;
+  rep(i, m) {
+    string s; cin >> s;
+    if (mp.find(s) == mp.end()) {
+      mp.insert(make_pair(s, -1));
+    } else {
+      mp[s]--;
+    }
+  }
+
+  int ans = 0;
+  for (auto it = mp.begin(); it != mp.end(); it++) {
+    // debug2(it->first, it->second);
+    ans = max(ans, it->second);
+  }
+  outl(ans);
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

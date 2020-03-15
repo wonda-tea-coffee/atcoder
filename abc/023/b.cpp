497894d5f2c8c9b86d988fc13545f4becab20277
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-15 22:17:22 +0900

    solve

---
+++ abc/023/b.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int n; cin >> n;
+  string s; cin >> s;
+
+  if (s == "b") {
+    outl(0);
+    return;
+  }
+
+  deque<char> deq;
+  deq.push_back('b');
+
+  for (int i = 1; ; i++) {
+    if (i % 3 == 0) {
+      deq.push_front('b');
+      deq.push_back('b');
+    } else if (i % 3 == 1) {
+      deq.push_front('a');
+      deq.push_back('c');
+    } else {
+      deq.push_front('c');
+      deq.push_back('a');
+    }
+
+    string t = string(deq.begin(), deq.end());
+    if (s == t) {
+      outl(i);
+      break;
+    }
+    if (t.size() > n) {
+      outl(-1);
+      break;
+    }
+  }
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

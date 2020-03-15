6cf1c1c35e5aea984c0ada4fd59fea7804d9acc8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-15 11:56:17 +0900

    solve

---
+++ abc/064/c.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int n; cin >> n;
+  int color[9] = {};
+  int lims[9] = {400, 800, 1200, 1600, 2000, 2400, 2800, 3200, __INT_MAX__};
+  rep(i, n) {
+    int a; cin >> a;
+    for (int j = 0; j < 9; j++) {
+      if (a < lims[j]) {
+        color[j]++;
+        break;
+      }
+    }
+  }
+
+  int minCnt = 0;
+  for (int i = 0; i < 8; i++) {
+    // debug2(i, color[i]);
+    if (color[i] > 0) minCnt++;
+  }
+  cout << max(minCnt, 1) << " " << minCnt + color[8] << endl;
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

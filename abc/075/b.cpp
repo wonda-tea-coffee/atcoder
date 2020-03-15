d2114bee5c975c419d8ca55b31e84c2b3f886869
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-15 11:27:25 +0900

    solve

---
+++ abc/075/b.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int h, w; cin >> h >> w;
+  vector<string> v(h); rep(i, h) cin >> v[i];
+
+  for (int hi = 0; hi < h; hi++) {
+    for (int wi = 0; wi < w; wi++) {
+      if (v[hi][wi] == '#') continue;
+
+      int count = 0;
+      for (int di = 0; di < 8; di++) {
+        int nhi = hi + dy[di];
+        int nwi = wi + dx[di];
+
+        if (nhi >= 0 && nhi < h && nwi >= 0 && nwi < w && v[nhi][nwi] == '#') count++;
+      }
+      v[hi][wi] = (char)('0' + count);
+    }
+  }
+
+  for (string vi : v) outl(vi);
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

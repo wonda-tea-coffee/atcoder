b8de1910ceacfff03bb0bdeb09ddbdd72d7da300
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-15 20:51:44 +0900

    solve

---
+++ abc/010/c.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ld dist(ld x1, ld y1, ld x2, ld y2) {
+  ld v1 = x1 - x2;
+  ld v2 = y1 - y2;
+  return sqrt(v1 * v1 + v2 * v2);
+}
+
+void solve() {
+  ld txa, tya, txb, tyb, T, V; cin >> txa >> tya >> txb >> tyb >> T >> V;
+  int n; cin >> n;
+  bool ans = false;
+
+  rep(i, n) {
+    ld xi, yi; cin >> xi >> yi;
+    ld d = dist(txa, tya, xi, yi) + dist(xi, yi, txb, tyb);
+    ans |= d <= T * V;
+  }
+
+  if (ans) YES();
+  else NO();
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

fdf88a630e881d9b00b618ed301c91d646e8eae8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-16 22:07:27 +0900

    solve

---
+++ arc/081/a.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll mypow(ll x, ll n) {
+  ll res = 1;
+  while (n > 0) {
+    if (n & 1) res = res * x;
+    x = x * x;
+    n >>= 1;
+  }
+  return res;
+}
+
+void solve() {
+  ll n; cin >> n;
+  map<ll, ll> map;
+  rep(i, n) {
+    ll a; cin >> a;
+    if (map.find(a) == map.end()) map.insert(make_pair(a, 1));
+    else map[a]++;
+  }
+  priority_queue<P> pq;
+  for (auto it = map.begin(); it != map.end(); it++)
+    if (it->second >= 2) pq.push(make_pair(it->first, it->second));
+  
+  ll ans = -1;
+  while (!pq.empty()) {
+    P p = pq.top(); pq.pop();
+    ll f = p.first;
+    ll s = p.second;
+    if (s >= 4) {
+      outl(mypow(f, 2));
+      return;
+    } else if (s >= 2) {
+      if (ans == -1) ans = f;
+      else {
+        outl(ans * f);
+        return;
+      }
+    }
+  }
+  outl(0);
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

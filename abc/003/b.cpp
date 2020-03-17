8dc51925c6f19f9821e190c6e9844ee25dc4a44a
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-17 23:10:58 +0900

    solve

---
+++ abc/003/b.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+bool isAtcoder(char c) {
+  vector<char> v = {'a', 't', 'c', 'o', 'd', 'e', 'r'};
+  for (char vi : v) if (c == vi) return true;
+  return false;
+}
+
+void solve() {
+  string s, t; cin >> s >> t;
+  int n = s.size();
+
+  for (int i = 0; i < n; i++) {
+    if (s[i] == t[i]) continue;
+    if (s[i] != '@' && t[i] != '@') {
+      outl("You will lose");
+      return;
+    } else if (s[i] == '@' && !isAtcoder(t[i])) {
+      outl("You will lose");
+      return;
+    } else if (t[i] == '@' && !isAtcoder(s[i])) {
+      outl("You will lose");
+      return;
+    }
+  }
+  outl("You can win");
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

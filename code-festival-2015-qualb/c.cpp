e1b24e8c38b3039807ac5c7e9640f58412ea7fd4
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-19 13:03:59 +0900

    solve

---
+++ code-festival-2015-qualb/c.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int n, m; cin >> n >> m;
+  vector<int> a(n), b(m);
+  rep(i, n) cin >> a[i];
+  rep(i, m) cin >> b[i];
+  sort(a); sort(b);
+
+  int ai = 0;
+  for (int bi = 0; bi < m; bi++) {
+    while (ai < n && b[bi] > a[ai]) ai++;
+    if (ai == n) {
+      NO();
+      return;
+    }
+    a[ai++] = 0;
+  }
+
+  YES();
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

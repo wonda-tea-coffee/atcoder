26b73146f6ce037bb2e5f2aa1dc27a30bad13d52
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-20 18:37:12 +0900

    solve

---
+++ arc/051/a.cpp
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll dist2(ll x1, ll y1, ll x2, ll y2) {
+  ll v1 = abs(x1 - x2);
+  ll v2 = abs(y1 - y2);
+  return v1 * v1 + v2 * v2;
+}
+
+void solve() {
+  int x1, y1, r; cin >> x1 >> y1 >> r;
+  int x2, y2, x3, y3; cin >> x2 >> y2 >> x3 >> y3;
+
+  if (x2 <= x1 - r && x1 + r <= x3 && y1 + r <= y3 && y2 <= y1 - r) NO();
+  else YES();
+
+  r *= r;
+  if (dist2(x1, y1, x2, y2) <= r &&
+      dist2(x1, y1, x2, y3) <= r &&
+      dist2(x1, y1, x3, y2) <= r &&
+      dist2(x1, y1, x3, y3) <= r) NO();
+  else YES();
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

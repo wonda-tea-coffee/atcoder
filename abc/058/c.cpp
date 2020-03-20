3d8a218967b16a484ecd31925bade00b0e99f8e9
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-20 19:17:53 +0900

    solve

---
+++ abc/058/c.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define sort(a)         sort((a).begin(), (a).end())
+#define uniq(a)         sort(a);(a).erase(unique((a).begin(), (a).end()), (a).end())
+#define reverse(a)      reverse((a).begin(), (a).end())
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+string repeat(char c, int t) {
+  string ret = "";
+  for (int i = 0; i < t; i++) ret += c;
+  return ret;
+}
+
+void solve() {
+  int n; cin >> n;
+  int cnts[50][26] = {};
+  rep(i, n) {
+    string s; cin >> s;
+    for (int j = 0; j < s.size(); j++)
+      cnts[i][s[j] - 'a']++;
+  }
+  string ans = "";
+  for (int j = 0; j < 26; j++) {
+    int cnt = __INT_MAX__;
+    for (int i = 0; i < n; i++) {
+      // debug3(j, i, cnts[i][j]);
+      if (cnts[i][j] == 0) {
+        cnt = 0;
+        break;
+      } else {
+        cnt = min(cnt, cnts[i][j]);
+      }
+    }
+    // debug2(j, cnt);
+    ans += repeat((char)('a' + j), cnt);
+  }
+  outl(ans);
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

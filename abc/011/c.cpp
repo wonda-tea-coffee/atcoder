4f1c2cb7539e11f3949c187ba1934afce9bd0c5b
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-21 20:06:34 +0900

    solve

---
+++ abc/011/c.cpp
@@ -0,0 +1,78 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int n, ng1, ng2, ng3; cin >> n >> ng1 >> ng2 >> ng3;
+  if (n == ng1 || n == ng2 || n == ng3) {
+    NO();
+    return;
+  }
+
+  const int MAX = 305;
+  vector<bool> dp(MAX, false);
+  dp[0] = true;
+
+  rep(i, 100) {
+    for (int i = n; i >= 1; i--) {
+      if (i == ng1 || i == ng2 || i == ng3) continue;
+      dp[i] = dp[i] || dp[i-1];
+      if (i-2>=0) dp[i] = dp[i] || dp[i-2];
+      if (i-3>=0) dp[i] = dp[i] || dp[i-3];
+    }
+  }
+ 
+  if (dp[n]) YES();
+  else NO();
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

a55eb647f06093b6e57a70361d7062f1d0026fb8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-21 23:25:27 +0900

    solve

---
+++ abc/033/c.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+vector<string> split(string s, char sep) {
+  vector<string> ret;
+  string tmp;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == sep) {
+      ret.push_back(tmp);
+      tmp = "";
+    } else {
+      tmp += s[i];
+    }
+  }
+  if (tmp.size() > 0) ret.push_back(tmp);
+  return ret;
+}
+
+void solve() {
+  string s; cin >> s;
+  vector<string> t = split(s, '+');
+  int ans = 0;
+  for (int ti = 0; ti < t.size(); ti++) {
+    vector<string> u = split(t[ti], '*');
+    int cnt = 0;
+    for (int ui = 0; ui < u.size(); ui++) {
+      if (u[ui] == "0") {
+        cnt = 0;
+        break;
+      } else {
+        cnt = 1;
+      }
+    }
+    ans += cnt;
+  }
+  outl(ans); 
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

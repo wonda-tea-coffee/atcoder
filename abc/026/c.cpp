89dab575bceb834dbca7bbd5757dab2a48c0d63a
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-21 21:26:13 +0900

    solve

---
+++ abc/026/c.cpp
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll n;
+vector<ll> b;
+ll memo[21] = {};
+
+ll dfs(int m) {
+  if (memo[m] > 0) return memo[m];
+
+  ll maxs = -1, mins = __INT_MAX__, cnt = 0;
+
+  for (int i = 1; i <= n; i++) {
+    if (i == m) continue;
+    if (b[i] == m) {
+      ll s = dfs(i);
+      maxs = max(maxs, s);
+      mins = min(mins, s);
+      cnt++;
+    }
+  }
+
+  if (cnt == 0) memo[m] = 1;
+  else if (cnt == 1) memo[m] = maxs * 2 + 1;
+  else memo[m] = maxs + mins + 1;
+
+  return memo[m];
+}
+
+void solve() {
+  cin >> n;
+  b.resize(n + 1);
+  for (int i = 2; i <= n; i++)
+    cin >> b[i];
+  
+  memset(memo, 0, sizeof(memo));
+  
+  outl(dfs(1));
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

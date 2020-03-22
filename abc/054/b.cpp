97fd5a4d7befc5fd19e8ee1c16cde0ecc79b7447
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-22 10:17:51 +0900

    solve

---
+++ abc/054/b.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int m, n; cin >> n >> m;
+  vector<string> a(n), b(m);
+  rep(i, n) cin >> a[i];
+  rep(i, m) cin >> b[i];
+
+  for (int i = 0; i <= n - m; i++) {
+    for (int j = 0; j <= n - m; j++) {
+      // debug2(i, j);
+      bool match = true;
+      for (int k = 0; k < m; k++) {
+        for (int l = 0; l < m; l++) {
+          if (b[k][l] != a[i+k][j+l]) {
+            // cout << "i = " << i << ", j = " << j << ", k = " << k << ", l = " << l << endl;
+            match = false;
+            break;
+          }
+        }
+        if (!match) break;
+      }
+      if (match) {
+        Yes();
+        return;
+      }
+    }
+  }
+  No();
+}
+
+int main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

0b885219011ae9b99cac4befbcc5e65052a7f4c7
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-23 23:04:40 +0900

    解説AC

---
+++ joi2008yo/d.cpp
@@ -0,0 +1,95 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int m; cin >> m;
+  vector<P> mv(m);
+  rep(i, m) {
+    int xi, yi; cin >> xi >> yi;
+    mv[i] = make_pair(xi, yi);
+  }
+
+  int n; cin >> n;
+  vector<P> nv(n);
+  rep(i, n) {
+    int xi, yi; cin >> xi >> yi;
+    nv[i] = make_pair(xi, yi);
+  }
+  sort(nv);
+
+  int x0 = mv[0].first;
+  int y0 = mv[0].second;
+  for (int i = 0; i < n; i++) {
+    int subx = nv[i].first - x0;
+    int suby = nv[i].second - y0;
+
+    bool b = true;
+
+    for (int j = 0; j < m; j++) {
+      if (!binary_search(all(nv), make_pair(mv[j].first + subx, mv[j].second + suby))) {
+        b = false;
+        break;
+      }
+    }
+
+    if (b) {
+      cout << subx << " " << suby << endl;
+      return;
+    }
+  }
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

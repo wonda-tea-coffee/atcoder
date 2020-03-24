0f8b9e533fc963288dc028af85ef130c67d6ac26
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-24 12:47:52 +0900

    solve

---
+++ abc/016/c.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  int n, m; cin >> n >> m;
+  const int MAX = 10;
+  bool rel[MAX + 1][MAX + 1] = {false};
+  rep(i, m) {
+    int a, b; cin >> a >> b;
+    rel[a][b] = true;
+    rel[b][a] = true;
+  }
+  for (int i = 1; i <= n; i++) {
+    set<int> friends;
+
+    for (int j = 1; j <= n; j++) {
+      if (!rel[i][j]) continue;
+      for (int k = 1; k <= n; k++) {
+        if (rel[j][k]) friends.insert(k);
+      }
+    }
+
+    // 自身を除外
+    friends.erase(i);
+
+    // 直接の友達を除外
+    for (int j = 1; j <= n; j++)
+      if (rel[i][j]) friends.erase(j);
+
+    outl(friends.size());
+  }
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

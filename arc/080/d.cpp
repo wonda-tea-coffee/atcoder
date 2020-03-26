4b459b21e24308e56153701bd10e2a0b5ac163a8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-26 20:37:27 +0900

    solve

---
+++ arc/080/d.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  ll h, w, n; cin >> h >> w >> n;
+  vector<ll> a(n); rep(i, n) cin >> a[i];
+
+  const int MAX = 100;
+  int grid[MAX][MAX] = {{}};
+
+  int i = 0, j = 0, cnt = 0, color = 0, dist = 1;
+  while (i < h) {
+    // debug3(i, j, color);
+    grid[i][j] = color;
+    cnt++;
+    if (a[color] == cnt) {
+      cnt = 0;
+      color++;
+    }
+    j += dist;
+    if (j == w || j == -1) {
+      i++;
+      j = j == w ? w - 1 : 0;
+      dist *= -1;
+    }
+  }
+
+  rep(i, h) {
+    cout << grid[i][0] + 1;
+    for (int j = 1; j < w; j++)
+      cout << " " << grid[i][j] + 1;
+    cout << endl;
+  }
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

c1163ed9dc38a021c8a6a5dbe32b19a6ac77af64
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-27 00:35:50 +0900

    solve

---
+++ abc/049/c.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+bool endwith(string s, string t) {
+  int tlen = t.size(), slen = s.size();
+  if (tlen > slen) return false;
+  int si = slen - 1;
+  for (int ti = tlen - 1, si = slen - 1; ti >= 0; ti--, si--) {
+    if (s[si] != t[ti]) return false;
+  }
+  return true;
+}
+
+void solve() {
+  string s; cin >> s;
+  vector<string> v = {"dream", "dreamer", "erase", "eraser"};
+  while (s.size() > 0) {
+    bool ok = false;
+    for (int i = 0; i < 4; i++) {
+      if (endwith(s, v[i])) {
+        s = s.substr(0, s.size() - v[i].size());
+        ok = true;
+        break;
+      }
+    }
+    if (!ok) {
+      NO();
+      return;
+    }
+  }
+  YES();
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

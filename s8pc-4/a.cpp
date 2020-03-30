dfa2227f4138052c53a0b4a563e1c0b3c7caa168
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-30 10:28:19 +0900

    解けねぇ！w

---
+++ s8pc-4/a.cpp
@@ -0,0 +1,94 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end()))
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+bool losable(string s, string t) {
+  ll slen = s.size(), tlen = t.size();
+  ll si = 0, ti = 0;
+  while (si < slen && ti < tlen) {
+    if (t[ti] == '?' && s[si] != 'z') return true;
+    else if (t[ti] != '?' && s[si] < t[ti]) return true;
+    si++; ti++;
+  }
+  if (si == ti) return false;
+  return slen < tlen;
+}
+
+template<class T>
+void outvo(vector<T> v) {
+  if (v.size() == 0) return;
+  cout << v[0];
+  for (int i = 1; i < v.size(); i++)
+    cout << " " << v[i];
+  cout << endl;
+}
+
+void solve() {
+  ll n; cin >> n;
+  vector<string> s(n); rep(i, n) cin >> s[i];
+  string t; cin >> t;
+
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (losable(t, s[i])) cnt++;
+  }
+
+  vector<ll> ans;
+  for (int i = 1 + cnt; i <= n+1; i++) ans.push_back(i);
+  outvo(ans);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

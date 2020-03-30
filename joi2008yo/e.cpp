2bd87475d97149c0100b5c8410a16ad368ecd40e
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-30 09:48:43 +0900

    solve

---
+++ joi2008yo/e.cpp
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll r, c;
+
+void mycopy(vector<vector<bool>> &from, vector<vector<bool>> &to) {
+  rep(i, r)rep(j, c) to[i][j] = from[i][j];
+}
+
+void solve() {
+  cin >> r >> c;
+  vector<vector<bool>> tbl(r);
+  rep(i, r) {
+    tbl[i].resize(c);
+    rep(j, c) {
+      ll tmp; cin >> tmp;
+      tbl[i][j] = tmp == 1;
+    }
+  }
+
+  ll ans = 0;
+  vector<vector<bool>> cpy(r); rep(i, r) cpy[i].resize(c);
+
+  for (int i = 0; i < (1<<r); i++) {
+    mycopy(tbl, cpy);
+
+    for (int j = 0; j < r; j++) {
+      if ((i >> j) & 1) {
+        for (int k = 0; k < c; k++) {
+          cpy[j][k] = !cpy[j][k];
+        }
+      }
+    }
+
+    ll sum = 0;
+    for (int ci = 0; ci < c; ci++) {
+      ll cnt = 0;
+      for (int ri = 0; ri < r; ri++) {
+        if (cpy[ri][ci]) cnt++;
+      }
+      sum += max(cnt, r-cnt);
+    }
+
+    ans = max(ans, sum);
+  }
+  outl(ans);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

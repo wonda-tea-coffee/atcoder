31ec25d0d2096d2681b4744f14c324d58574cd35
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-30 22:38:20 +0900

    solve

---
+++ joi2009ho/a.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  ll n, m; cin >> n >> m;
+  string s; cin >> s;
+  ll ans = 0, cnt = 0;
+  rep(i, m) {
+    if (cnt % 2 == 0 && s[i] == 'I') cnt++;
+    else if (cnt % 2 == 1 && s[i] == 'O') cnt++;
+    else {
+      // debug3(i, cnt, ans);
+      ans += max(0ll, (cnt-1)/2 - n + 1);
+
+      if (s[i] == 'I') cnt = 1;
+      else cnt = 0;
+    }
+  }
+
+  outl(ans + max(0ll, (cnt-1)/2 - n + 1));
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

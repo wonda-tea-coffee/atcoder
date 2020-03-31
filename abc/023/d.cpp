267ef00a26bbec88141b5498cf9e966175f1b2f8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-03-31 09:20:17 +0900

    解説AC

---
+++ abc/023/d.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll n;
+const ll MAX_N = 100000;
+ll h[MAX_N], s[MAX_N];
+
+bool check(ll x) {
+  vector<ll> t(n);
+  rep(i, n) {
+    if (h[i] > x) return false;
+    t[i] = (x - h[i]) / s[i];
+  }
+  sort(t);
+  rep(i, n) {
+    if (t[i] < i) return false;
+  }
+  return true;
+}
+
+void solve() {
+  cin >> n;
+  rep(i, n) cin >> h[i] >> s[i];
+
+  ll ok = 110000000000000, ng = 0;
+  while (ok - ng > 1) {
+    ll mid = (ok + ng) / 2;
+    // debug3(ng, mid, ok);
+    if (check(mid)) ok = mid;
+    else ng = mid;
+  }
+  outl(ok);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

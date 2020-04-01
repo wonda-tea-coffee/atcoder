988e5d8fb05dbdb68b7f6280184250f876d17668
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-01 21:18:03 +0900

    solve

---
+++ abc/075/c_uf.cpp
@@ -0,0 +1,121 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+class DisjointSet {
+  public:
+    vector<ll> rank, p;
+
+    DisjointSet() {}
+    DisjointSet(ll size) {
+      rank.resize(size, 0);
+      p.resize(size, 0);
+      rep(i, size) p[i] = i;
+    }
+
+    bool same(ll x, ll y) {
+      return find_set(x) == find_set(y);
+    }
+
+    void unite(ll x, ll y) {
+      link(find_set(x), find_set(y));
+    }
+
+    void link(ll x, ll y) {
+      if (x == y) return;
+
+      if (rank[x] > rank[y]) {
+        p[y] = x;
+      } else {
+        p[x] = y;
+        if (rank[x] == rank[y]) rank[y]++;
+      }
+    }
+
+    ll find_set(ll x) {
+      if (x != p[x]) {
+        p[x] = find_set(p[x]);
+      }
+      return p[x];
+    }
+};
+
+void solve() {
+  ll n, m; cin >> n >> m;
+  vector<P> e(m);
+  rep(i, m) {
+    cin >> e[i].first >> e[i].second;
+    e[i].first--;
+    e[i].second--;
+  }
+
+  ll ans = 0;
+  for (int i = 0; i < m; i++) {
+    DisjointSet ds = DisjointSet(n);
+    for (int j = 0; j < m; j++) {
+      if (i != j && !ds.same(e[j].first, e[j].second)) {
+        ds.unite(e[j].first, e[j].second);
+      }
+    }
+
+    set<ll> set;
+    rep(j, n) set.insert(ds.find_set(j));
+    if (set.size() > 1) ans++;
+  }
+  outl(ans);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

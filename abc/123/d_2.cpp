4b6f62e751ab61ac8602da642a7ec9b872403b68
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-05 22:45:50 +0900

    solve

---
+++ abc/123/d_2.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+void solve() {
+  ll X, Y, Z, K; cin >> X >> Y >> Z >> K;
+  vector<ll> a(X), b(Y), c(Z);
+  rep(i, X) cin >> a[i];
+  rep(i, Y) cin >> b[i];
+  rep(i, Z) cin >> c[i];
+  sort(a); sort(b); sort(c);
+
+  vector<ll> v;
+  for (int i = 1; i <= min(X, K); i++) {
+    for (int j = 1; j <= Y; j++) {
+      if (i * j > K) break;
+      for (int k = 1; k <= Z; k++) {
+        if (i * j * k > K) break;
+        // debug3(a[X-i], b[Y-j], c[Z-k]);
+        v.emplace_back(a[X-i] + b[Y-j] + c[Z-k]);
+      }
+    }
+  }
+  sort(v);
+  ll vi = v.size() - 1;
+  rep(i, K) outl(v[vi-i]);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

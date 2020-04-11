8af0bd8ef01efc1d71d538287431fe1623db10b0
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-11 19:25:32 +0900

    solve

---
+++ past201912-open/f.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+// #define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+bool asc(const string &s, const string &t) {
+  ll slen = s.size(), tlen = t.size();
+
+  for (ll si = 0, ti = 0; si < slen && ti < tlen; si++, ti++) {
+    char S = s[si], T = t[ti];
+    if (S < 97) S += 32;
+    if (T < 97) T += 32;
+    if (S < T) return true;
+    else if (S > T) return false;
+  }
+  return slen < tlen;
+}
+
+void solve() {
+  string s; cin >> s;
+  vector<string> v;
+
+  int i = 0, n = s.size();
+  while (i < n) {
+    int j = i + 1;
+    while (j < n && 'a' <= s[j] && s[j] <= 'z') j++;
+    // debug3(i, j, s.substr(i, j-i+1));
+    v.emplace_back(s.substr(i, j-i+1));
+    i = j+1;
+  }
+
+  sort(all(v), asc);
+  for (string vi : v) cout << vi;
+  cout << endl;
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

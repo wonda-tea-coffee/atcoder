cafa8c217dc59c35042d789d036624f5df2bb1f8
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-12 21:49:24 +0900

    解けねぇ！w

---
+++ abc/162/e/e.cpp
@@ -0,0 +1,87 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <limits>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <regex>
+#include <vector>
+
+#define fix(n)          cout<<fixed<<setprecision(n)
+#define rep(i,n)        for (int i = 0; i < (n); ++i)
+#define all(a)          (a).begin(), (a).end()
+#define sort(a)         sort(all(a))
+#define uniq(a)         sort(a);(a).erase(unique(all(a)), (a).end())
+#define reverse(a)      reverse(all(a))
+#define ctos(c)         string(1, (c))
+#define out(d)          cout << (d)
+#define outl(d)         std::cout<<(d)<<"\n"
+#define YES()           cout << "YES" << endl
+#define NO()            cout << "NO" << endl
+#define Yes()           cout << "Yes" << endl
+#define No()            cout << "No" << endl
+#define ceil(x, y)      ((x + y - 1) / (y))
+#define debug(x)        cerr << #x << ": " << (x) << '\n'
+#define debug2(x, y)    cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << '\n'
+#define debug3(x, y, z) cerr << #x << ": " << (x) << ", " << #y << ": " << (y) << ", " << #z << ": " << (z) << '\n'
+#define dbg(v)          for (size_t _ = 0; _ < v.size(); ++_){ cerr << #v << "[" << _ << "] : " << v[_] << '\n'; }
+#define pb              push_back
+#define fst             first
+#define int             long long
+#define INF             __LONG_LONG_MAX__
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+using P = pair<ll,ll>;
+
+const ll MOD = 1000000007; // 10^9 + 7
+const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
+const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
+
+ll gcd(ll a, ll b) {
+  if (a < b) return gcd(b, a);
+  if (b == 0) return a;
+  return gcd(b, a % b);
+}
+
+void solve() {
+  // ll n, k; cin >> n >> k;
+  ll k = 200, ans = 0;
+  vector<vector<string>> v(k+1);
+  for (int a = 1; a <= k; a++) {
+    for (int b = 1; b <= k; b++) {
+      for (int c = 1; c <= k; c++) {
+        ll g = gcd(gcd(a, b), c);
+        v[g].emplace_back(to_string(a) + "," + to_string(b) + "," + to_string(c));
+        ans += g;
+      }
+    }
+  }
+  for (int i = 1; i <= k; i++) {
+    outl(i);
+    for (int j = 0; j < v[i].size(); j++) {
+      outl(v[i][j]);
+    }
+  }
+  outl(ans);
+}
+
+signed main() {
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  srand((unsigned)time(NULL));
+  fix(12);
+
+  solve();
+}

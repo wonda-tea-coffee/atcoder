4013a85c74c34e3e0afed683b4d7fa11aaa9ecfe
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2020-04-12 21:20:30 +0900

    solve

--- abc/162/d/d.cpp
+++ abc/162/d/d.cpp
@@ -50,7 +50,34 @@ const int dx[8] = {1, 0, -1, 0, 1, -1, -1, 1};
 const int dy[8] = {0, 1, 0, -1, 1, 1, -1, -1};
 
 void solve() {
-  
+  ll n; cin >> n;
+  string s; cin >> s;
+  vector<ll> cntr(n+1), cntg(n+1), cntb(n+1);
+  for (int i = n-1; i >= 0; i--) {
+    cntr[i] = cntr[i+1];
+    cntg[i] = cntg[i+1];
+    cntb[i] = cntb[i+1];
+    if (s[i] == 'R') cntr[i]++;
+    if (s[i] == 'G') cntg[i]++;
+    if (s[i] == 'B') cntb[i]++;
+  }
+  ll ans = 0;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      if (s[i] == s[j]) continue;
+      ll v;
+      char c;
+      if (s[i] == 'R' && s[j] == 'G') {v = cntb[j+1];c='B';}
+      if (s[i] == 'G' && s[j] == 'R') {v = cntb[j+1];c='B';}
+      if (s[i] == 'R' && s[j] == 'B') {v = cntg[j+1];c='G';}
+      if (s[i] == 'B' && s[j] == 'R') {v = cntg[j+1];c='G';}
+      if (s[i] == 'B' && s[j] == 'G') {v = cntr[j+1];c='R';}
+      if (s[i] == 'G' && s[j] == 'B') {v = cntr[j+1];c='R';}
+      ans += v;
+      if (2 * j - i < n && s[2 * j - i] == c) ans--;
+    }
+  }
+  outl(ans);
 }
 
 signed main() {
